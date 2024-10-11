546e534aeb7a7a0fc98c5c0573db0bd873db3cfd
nofiiy <163719633+nofiiy@users.noreply.github.com>
2024-10-11 10:01:49 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,5 +1,1 @@
-function loginSuccess(event) {
-    event.preventDefault(); // Mencegah form submit secara otomatis
-    alert("Login berhasil! Anda akan diarahkan ke halaman berikutnya.");
-    window.location.href = "semuakelas.html";
-}
+
