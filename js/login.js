4b5a27c844578a4184d5ce63be3af4041ab7dc5d
ReisyeRema <reisyerema19@gmail.com>
2024-10-17 16:17:29 +0700

    Memperbaiki Halaman Login

--- js/login.js
+++ js/login.js
@@ -1,25 +1,30 @@
 $(document).ready(function() {     
+    
     // Animasi hover pada tombol login
     $('.btn-primary').hover(
         function() {
+            // Ketika kursor diarahkan pada tombol, ubah opacity dan ukuran font
             $(this).animate({opacity: 0.8, fontSize: '1.1em'}, 200);
         },
         function() {
+            // Ketika kursor meninggalkan tombol, kembalikan opacity dan ukuran font ke nilai semula
             $(this).animate({opacity: 1, fontSize: '1em'}, 200);
         }
     );
 });
 
 function loginSuccess(event) {
-    event.preventDefault(); // Mencegah form submit secara otomatis
+    event.preventDefault(); 
 
+    // Mendapatkan nilai input dari kolom username dan password
     var username = $('input[type="text"]').val();
     var password = $('input[type="password"]').val();
 
+    // Validasi apakah username atau password kosong
     if (username === "" || password === "") {
-        alert("Username dan password harus diisi!");
+        alert("Username dan password harus diisi!"); // Menampilkan pesan peringatan jika ada kolom yang kosong
     } else {
-        alert("Login berhasil! Anda akan diarahkan ke halaman berikutnya.");
+        alert("Login berhasil! Anda akan diarahkan ke halaman berikutnya."); 
         window.location.href = "semuakelas.html"; 
     }
 }
